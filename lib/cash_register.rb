8433890923b2ea45320133ff5e7ee96a59d9b602
sasha walker <sashawalker31@gmail.com>
2020-02-15 17:09:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,39 +1,40 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
-
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
+    attr_accessor :total, :items, :discount, :last_transaction_amount 
+    def initialize(total = 0, discount = 20)
+        @total = total
+        @discount = discount
+        @items =[]
     end
-  end
 
-  def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
-      return "There is no discount to apply."
+    def add_item(item, price, qty = 1)
+        qty.times do
+            self.items << item
+        end
+        @total += (price * qty)
+
+        @last_transaction_amount = @total
     end
-  end
 
-  def void_last_transaction
-    @total -= @price
-  end
+    def apply_discount
+        if self.discount
+            discount = @discount/100.to_f
+            @total = @total - (@total * discount)
+            # binding.pry
+            return "After the discount, the total comes to #{self.total}"
+        else
+            puts "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+
+        @total -= @last_transaction_amount
+
+    end
+
+
+
+
 
 end
